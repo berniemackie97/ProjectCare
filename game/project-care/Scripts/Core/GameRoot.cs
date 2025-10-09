c4b0e215377af1be911394218cebcba6ab7579c8
Dave <davidglogowski2003@gmail.com>
2025-10-09 14:37:13 +0200

    feat(title): Add title screen with start game button

---
+++ game/project-care/Scripts/Core/GameRoot.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+namespace ProjectCare.Scripts.Core;
+
+public partial class GameRoot : Node
+{
+    private Node _holder;
+    private Node _current;
+
+    private PackedScene _titleScene = GD.Load<PackedScene>("res://Scenes/Title/TitleScreen.tscn");
+    private PackedScene _gameScene  = GD.Load<PackedScene>("res://Scenes/Game/Game.tscn");
+
+    public override void _Ready()
+    {
+        _holder = GetNode("SceneHolder");
+        LoadTitle();
+    }
+
+    private void SwapTo(PackedScene scene)
+    {
+        _current?.QueueFree();
+        _current = scene.Instantiate();
+        _holder.AddChild(_current);
+    }
+
+    public void LoadTitle() => SwapTo(_titleScene);
+    public void LoadGame()  => SwapTo(_gameScene);
+    
+}
