7145cbcf0c215d8ea87b9897aacfec5991d3ec05
Dave <davidglogowski2003@gmail.com>
2025-10-13 18:46:30 +0200

    feat(inventory): Add ItemDB and adding to inventory

--- game/project-care/Scripts/GameState/Inventory/Inventory.cs
+++ game/project-care/Scripts/GameState/Inventory/Inventory.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Godot;
+using ProjectCare.Scripts.Enums;
+using ProjectCare.Scripts.Resources.Inventory;
 
 namespace ProjectCare.Scripts.GameState;
 
@@ -6,23 +9,79 @@ public partial class Inventory : Control
 {
 
     private GridContainer _gridContainer;
-    private PackedScene inventoryButton;
-    [Export] private string itemButtonPath = "res://Scenes/Inventory/inventory_button.tscn";
-    [Export] public int Capacity { get; set; } = 24;
+    [Export] private PackedScene inventoryButtonScene;
+    [Export] public int Capacity { get; set; } = 18;
+
+    private List<ItemSlot> _slots = new();
     public override void _Ready()
     {
         _gridContainer = GetNode<GridContainer>("NinePatchRect/Grid");
-        inventoryButton = ResourceLoader.Load<PackedScene>(itemButtonPath);
+        populateSlots();
         populateButtons();
+        setupInitialItems();
+    }
+
+    private void setupInitialItems()
+    {
+        Add(GameDB.Instance.GetById(0), 10);
     }
 
     private void populateButtons()
     {
         for (int i = 0; i < Capacity; i++)
         {
-            InventoryButton currentInventoryButton = inventoryButton.Instantiate<InventoryButton>();
+            InventoryButton currentInventoryButton = inventoryButtonScene.Instantiate<InventoryButton>();
             _gridContainer.AddChild(currentInventoryButton);
+            UpdateButton(i);
         }
+    }
+
+    private void populateSlots()
+    {
+        for (int i = 0; i < Capacity; i++)
+        {
+            _slots.Add(new ItemSlot());
+        }
+    }
+
+    public int Add(ItemDef itemDef, int amount = 1) {
+        
+        if (itemDef == null || amount <= 0) return amount;
+
+        for (int i = 0; i < _slots.Count && amount > 0; i++) {
+            amount = _slots[i].TryAddToExistingSlot(itemDef, amount);
+            UpdateButton(i);
+        }
+
+        for (int i = 0; i < _slots.Count && amount > 0; i++) {
+            amount = _slots[i].FillEmpty(itemDef, amount);
+            UpdateButton(i);
+        }
+
+        return amount;
+    }
+
+    public void UpdateButton(int index)
+    {
+        InventoryButton button = getButtonInGrid(index);
+        ItemSlot itemSlot = _slots[index];
+
+        if (itemSlot.Id == -1 || itemSlot.Count == 0)
+        {
+            button.clear();
+            return;
+        }
+
+        ItemDef itemDef = itemSlot.GetItemDef();
+        button.SetIcon(itemDef?.Icon);
+        button.SetCount(itemSlot.Count);
+    }
+
+    private InventoryButton getButtonInGrid(int index) => _gridContainer.GetChild<InventoryButton>(index);
+
+    public void _on_add_button_button_down()
+    {
+        
     }
 
     public override void _Process(double delta)
--- game/project-care/Scripts/GameState/Inventory/ItemSlot.cs
+++ game/project-care/Scripts/GameState/Inventory/ItemSlot.cs
@@ -16,6 +16,36 @@ public class ItemSlot
     {
         return GameDB.Instance.GetById(Id.Value);
     }
+
+    public bool IsCorrectItem(int itemId) => this.Id == itemId;
+    public bool IsFull (int stackSize) => this.Count >= stackSize;
+    public bool IsEmpty() => Id == -1 || Count == 0;
+
+    public int GetAllowedAddAmount(ItemDef itemDef, int amount) => Mathf.Min(itemDef.StackSize - Count, amount);
+
+    public int TryAddToExistingSlot(ItemDef def, int amount)
+    {
+        if (!IsCorrectItem(def.Id) || IsFull(def.StackSize)) return amount;
+        int canAdd = GetAllowedAddAmount(def, amount);
+        Count += canAdd;
+        return amount - canAdd;
+    }
     
+    public int FillEmpty(ItemDef itemDef, int amount)
+    {
+        if (!IsEmpty()) return amount;
+        int toPlace = Mathf.Min(itemDef.StackSize, amount);
+        Id = itemDef.Id;
+        Count = toPlace;
+        return amount - toPlace;
+    }
     
+    public void Clear()
+    {
+        Id = -1;
+        Count = 0;
+    }
+
+
+
 }
